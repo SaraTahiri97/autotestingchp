ee221167c9905af39b993d4981dfa58d29c3fdac
SARA TAHIRI <saratahiri356@gmail.com>
2021-10-21 13:09:58 +0100

    initial_commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SaraTahiri97/autotestingchp
+
+go 1.17
---
+++ join.go
@@ -0,0 +1,18 @@
+package autotestingchp
+
+import (
+	"strings"
+)
+
+// Library to Join Multiple string values ( ex: if 2 : apples and oranges . if multipule : apples, oranges and pear)
+func JoinWithCommas(phrases []string) string {
+	/*
+		The code makes use of the strings.Join function,which takes a slice of strings and a string to join
+		them all together with. Join returns a single string with all the items from the slice combined,
+		with the joining string separating each entry.
+	*/
+	res := strings.Join(phrases[:len(phrases)-1], ", ") // phrases[:len(phrases)-1] : gives all but the last one
+	res += "and "                                       // add the and
+	res += phrases[len(phrases)-1]                      // gives the last one
+	return res
+}
