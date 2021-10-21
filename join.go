6e7b59ed9908f56441719511ae625a80399d3c7f
SARA TAHIRI <saratahiri356@gmail.com>
2021-10-21 16:04:09 +0100

    last_commit

--- join.go
+++ join.go
@@ -11,16 +11,17 @@ func JoinWithCommas(phrases []string) string {
 		them all together with. Join returns a single string with all the items from the slice combined,
 		with the joining string separating each entry.
 	*/
-	if len(phrases) > 1 {
-		if len(phrases) == 2 {
-			return phrases[0] + " and " + phrases[1]
-		} else {
-			res := strings.Join(phrases[:len(phrases)-1], ",") // phrases[:len(phrases)-1] : gives all but the last one
-			res += ", and "
-			res += phrases[len(phrases)-1] // gives the last one
-			return res
-		}
-	} else {
+	if len(phrases) == 0 {
+		return "" // to avoid the panic err of trying to access elements from an empty slice
+	} else if len(phrases) == 1 {
 		return phrases[0]
+	} else if len(phrases) == 2 {
+		return phrases[0] + " and " + phrases[1]
+	} else {
+		res := strings.Join(phrases[:len(phrases)-1], ",") // phrases[:len(phrases)-1] : gives all but the last one
+		res += ", and "
+		res += phrases[len(phrases)-1] // gives the last one
+		return res
 	}
+
 }
